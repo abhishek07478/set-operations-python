1f838451029eb7867852ea77aaef0078130302ca
abhishek07478 <sp2521@srmist.edu.in>
2026-03-17 09:00:47 +0530

    Replace Java code with Python Fibonacci function

--- PS1.java
+++ PS1.java
@@ -1,13 +1,15 @@
-class TestString {
-    public static void main(String[] args) {
-
-        String name = "Hello";
-
-        String arg1 = args[0];
-        String arg2 = args[1];
-
-        System.out.println(arg1);
-        System.out.println(arg2);
-        System.out.println(name);
-    }
-}
+def fibonacci(n):
+    a, b = 0, 1
+    if n <= 0:
+        print("Please enter a positive number")
+    elif n == 1:
+        print("Fibonacci series:")
+        print(a)
+    else:
+        print("Fibonacci series:")
+        for _ in range(n):
+            print(a, end=" ")
+            a, b = b, a + b
+if _name_ == "_main_":
+    n = int(input("Enter number of terms: "))
+    fibonacci(n)
