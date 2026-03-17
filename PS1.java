54609540c630d08c26f2d060821a0d119707d8b6
abhishek07478 <sp2521@srmist.edu.in>
2026-03-17 08:59:44 +0530

    Add files via upload

---
+++ PS1.java
@@ -0,0 +1,13 @@
+class TestString {
+    public static void main(String[] args) {
+
+        String name = "Hello";
+
+        String arg1 = args[0];
+        String arg2 = args[1];
+
+        System.out.println(arg1);
+        System.out.println(arg2);
+        System.out.println(name);
+    }
+}
